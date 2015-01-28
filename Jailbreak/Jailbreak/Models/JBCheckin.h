4e5d43e1fc59e6361a079930999e15042bd75628
Shayan Yousefizadeh <yousefs@tcd.ie>
2015-01-28 18:07:18 +0000

    Checkin object

---
+++ Jailbreak/Jailbreak/Models/JBCheckin.h
@@ -0,0 +1,24 @@
+//
+//  JBCheckin.h
+//  Jailbreak
+//
+//  Created by Shayan Yousefizadeh on 28/01/2015.
+//  Copyright (c) 2015 Jailbreak HQ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface JBCheckin : NSObject <NSCoding>
+
+@property (nonatomic, assign) NSUInteger ID;
+@property (nonatomic, strong) NSString *location;
+@property (nonatomic, strong) NSString *status;
+@property (nonatomic, assign) CLLocationDegrees latitude;
+@property (nonatomic, assign) CLLocationDegrees longitude;
+@property (nonatomic, assign) NSUInteger time;
+@property (nonatomic, assign) NSUInteger teamID;
+
+- (instancetype)initWithJSON:(NSDictionary *)json;
+
+@end
---
+++ Jailbreak/Jailbreak/Models/JBCheckin.m
@@ -0,0 +1,64 @@
+//
+//  JBCheckin.m
+//  Jailbreak
+//
+//  Created by Shayan Yousefizadeh on 28/01/2015.
+//  Copyright (c) 2015 Jailbreak HQ. All rights reserved.
+//
+
+#import "JBCheckin.h"
+
+@implementation JBCheckin
+
+#pragma mark - NSCoding
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super init];
+    
+    if (self)
+    {
+        self.ID = [aDecoder decodeIntegerForKey:@"ID"];
+        self.location = [aDecoder decodeObjectForKey:@"location"];
+        self.status = [aDecoder decodeObjectForKey:@"status"];
+        self.latitude = [aDecoder decodeDoubleForKey:@"latitude"];
+        self.longitude = [aDecoder decodeDoubleForKey:@"longitude"];
+        self.time = [aDecoder decodeIntegerForKey:@"time"];
+        self.teamID = [aDecoder decodeIntegerForKey:@"teamID"];
+    }
+    
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+    [aCoder encodeInteger:self.ID forKey:@"ID"];
+    [aCoder encodeObject:self.location forKey:@"location"];
+    [aCoder encodeObject:self.status forKey:@"status"];
+    [aCoder encodeDouble:self.latitude forKey:@"latitude"];
+    [aCoder encodeDouble:self.longitude forKey:@"longitude"];
+    [aCoder encodeInteger:self.time forKey:@"time"];
+    [aCoder encodeInteger:self.teamID forKey:@"teamID"];
+}
+
+#pragma mark - NSObject
+
+- (instancetype)initWithJSON:(NSDictionary *)json
+{
+    self = [super init];
+    
+    if (self)
+    {
+        self.ID = [json[@"id"] unsignedIntegerValue];
+        self.location = json[@"location"];
+        self.status = json[@"status"];
+        self.latitude = [json[@"lat"] doubleValue];
+        self.longitude = [json[@"lon"] doubleValue];
+        self.time = [json[@"time"] unsignedIntegerValue];
+        self.teamID = [json[@"team_id"] unsignedIntegerValue];
+    }
+    
+    return self;
+}
+
+@end
